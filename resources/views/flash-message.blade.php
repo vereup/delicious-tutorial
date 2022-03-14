e43a66900764e448b062c71fc022533c81432ad1
Euiyoung <justinko@amaxg.net>
2022-03-14 20:11:34 +0900

    session 수정중

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -1,7 +1,7 @@
 @if ($message = Session::get('success'))
 <div class="alert alert-success alert-block">
     <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong></strong>
+    <strong>{{ $message }}</strong>
 </div>
 @endif
 @if ($message = Session::get('error'))
